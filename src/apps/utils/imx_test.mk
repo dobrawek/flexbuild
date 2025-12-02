83bf43f980a2feb4271ccd198d967d4482f6cec9
Przemyslaw Dobrowolski <dobrawek@gmail.com>
2025-12-02 23:55:13 +0100

    tomorrow will be a day

--- src/apps/utils/imx_test.mk
+++ src/apps/utils/imx_test.mk
@@ -23,8 +23,8 @@ imx_test:
 	 sudo cp -rf $(DESTDIR)/usr/include/alsa $(RFSDIR)/usr/include && \
 	 cd $(UTILSDIR)/imx_test && \
 	 mkdir -p $(DESTDIR)/opt/unit_tests && \
-	 export CC="$(CROSS_COMPILE)gcc --sysroot=$(RFSDIR) -B$(RFSDIR)/usr/lib/aarch64-linux-gnu -I$(RFSDIR)/usr/include/aarch64-linux-gnu -I$(RFSDIR)/usr/include -I$(DESTDIR)/usr/include -O2 -pipe -g" && \
-	 export CXX="$(CROSS_COMPILE)g++ --sysroot=$(RFSDIR) -B$(RFSDIR)/usr/lib/aarch64-linux-gnu -I$(RFSDIR)/usr/include/aarch64-linux-gnu -I$(RFSDIR)/usr/include -I$(DESTDIR)/usr/include -L$(RFSDIR)/usr/lib/aarch64-linux-gnu" && \
+	 export CC="$(CROSS_COMPILE)gcc --sysroot=$(RFSDIR) -B$(RFSDIR)/usr/lib/aarch64-linux-gnu -I$(RFSDIR)/usr/include/aarch64-linux-gnu -I$(RFSDIR)/usr/include -I$(DESTDIR)/usr/include -O2 -pipe -g -L$(RFSDIR)/usr/lib/aarch64-linux-gnu -L$(RFSDIR)/lib/aarch64-linux-gnu -Wl,-rpath-link,$(RFSDIR)/usr/lib/aarch64-linux-gnu -Wl,-rpath-link,$(RFSDIR)/lib/aarch64-linux-gnu" && \
+	 export CXX="$(CROSS_COMPILE)g++ --sysroot=$(RFSDIR) -B$(RFSDIR)/usr/lib/aarch64-linux-gnu -I$(RFSDIR)/usr/include/aarch64-linux-gnu -I$(RFSDIR)/usr/include -I$(DESTDIR)/usr/include -L$(RFSDIR)/usr/lib/aarch64-linux-gnu -L$(RFSDIR)/lib/aarch64-linux-gnu -Wl,-rpath-link,$(RFSDIR)/usr/lib/aarch64-linux-gnu -Wl,-rpath-link,$(RFSDIR)/lib/aarch64-linux-gnu $(RFSDIR)/usr/lib/aarch64-linux-gnu/libstdc++.so.6" && \
 	 export LDFLAGS="-L$(RFSDIR)/usr/lib/aarch64-linux-gnu -L$(RFSDIR)/lib/aarch64-linux-gnu -Wl,-rpath-link,$(RFSDIR)/usr/lib/aarch64-linux-gnu -Wl,-rpath-link,$(RFSDIR)/lib/aarch64-linux-gnu" && \
 	 V=0 VERBOSE='' SDKTARGETSYSROOT=$(DESTDIR) PLATFORM=$(PLATFORM) \
 	 $(MAKE) -j$(JOBS) && \
